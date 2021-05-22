0aa5a2d0cfeed9ddce0d31eb97cc06d9930cd84a
Md. Pial Ahamed <root.pial@gmail.com>
2021-05-23 01:02:55 +0600

    Add new pages

---
+++ pages/thoughts.jsx
@@ -0,0 +1,11 @@
+import Posts from "../component/Posts";
+import Post from "../component/Post";
+
+export default function Thoughts() {
+  return (
+    <div>
+      <Post />
+      <Posts />
+    </div>
+  );
+}
