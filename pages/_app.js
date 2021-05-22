d5c85b56d5f8b7dcd9cfc5719d23bc193934660b
Md. Pial Ahamed <root.pial@gmail.com>
2021-05-22 20:33:43 +0600

    Add NavBar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.scss'
+import "../styles/globals.scss";
+import NavBar from "../component/NavBar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar signInStatus="false" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
