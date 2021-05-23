72b83b0ab3b327c2b11465aa5dfdaadc643f06a9
Md. Pial Ahamed <root.pial@gmail.com>
2021-05-23 11:21:18 +0600

    Update page

--- pages/one-minute.jsx
+++ pages/one-minute.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import TipsCard from "../component/TipsCard";
+import data from "../data/tips.json"
+import styles from "../styles/SignInSignUp.module.scss";
 export default function OneMinute() {
+  const [index, setIndex] = useState(0);
   return (
     <div>
-      <p>One Minute Stress</p>
+      <TipsCard
+        title={data[index].title}
+        description={data[index].description}
+        index={index}
+        setIndex={setIndex}
+      />
     </div>
   );
 }
